3ae8bf4097521bae1c43e7a5d439fc77e0e76e7f
Jacqueline Cournoyer <jcournoyer5@gmail.com>
2020-12-27 19:47:53 +0000

    Done.

--- lib/person.rb
+++ lib/person.rb
@@ -1,7 +1,21 @@
 # your code goes here
+
+class Person
+    
+    def initialize(name,bank_account,happiness,hygiene)
+    @name = name
+    @bank_account = bank_account
+    @happiness = happiness
+    @hygiene = hygiene
+    end
+    
+   stella = Person.new
+   stella.name = "Stella" 
+    
 def take_bath
         
         self.hygiene += 4
     
         "♪ Rub-a-dub just relaxing in the tub ♫"
+end
 end
